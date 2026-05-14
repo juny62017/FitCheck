8750d0de71adb3e0c3d9210ca2ae28b780824abd
juny62017 <juny62017@gmail.com>
2026-05-14 18:50:29 +0530

    Added BMI calculation and health result system

--- script.js
+++ script.js
@@ -4,4 +4,125 @@ let calculateBtn =
 let resetBtn =
     document.querySelector(".reset-btn");
 
-console.log("FitCheck Started");
+let heightInput =
+    document.getElementById("heightInput");
+
+let weightInput =
+    document.getElementById("weightInput");
+
+let ageInput =
+    document.getElementById("ageInput");
+
+let resultNumber =
+    document.querySelector(".result-number");
+
+let resultText =
+    document.querySelector(".result-text");
+
+calculateBtn.onclick = function () {
+
+    let heightValue =
+        heightInput.value;
+
+    let weightValue =
+        weightInput.value;
+
+    if (
+        heightValue === "" ||
+        weightValue === ""
+    ) {
+
+        resultNumber.innerText =
+            "00.0";
+
+        resultText.innerText =
+            "Please fill all fields";
+
+        resultText.style.color =
+            "#d67b7b";
+
+        return;
+    }
+
+    let heightInMeter =
+        Number(heightValue) / 100;
+
+    let weightInKg =
+        Number(weightValue);
+
+    let bmi =
+        weightInKg /
+        (heightInMeter * heightInMeter);
+
+    let finalBmi =
+        bmi.toFixed(1);
+
+    resultNumber.innerText =
+        finalBmi;
+
+    if (bmi < 18.5) {
+
+        resultText.innerText =
+            "Underweight";
+
+        resultText.style.color =
+            "#d8a657";
+
+    }
+
+    else if (
+        bmi >= 18.5 &&
+        bmi <= 24.9
+    ) {
+
+        resultText.innerText =
+            "Healthy Range";
+
+        resultText.style.color =
+            "#7da67d";
+
+    }
+
+    else if (
+        bmi >= 25 &&
+        bmi <= 29.9
+    ) {
+
+        resultText.innerText =
+            "Overweight";
+
+        resultText.style.color =
+            "#d8a657";
+
+    }
+
+    else {
+
+        resultText.innerText =
+            "Obese";
+
+        resultText.style.color =
+            "#d67b7b";
+
+    }
+
+};
+
+resetBtn.onclick = function () {
+
+    heightInput.value = "";
+
+    weightInput.value = "";
+
+    ageInput.value = "";
+
+    resultNumber.innerText =
+        "00.0";
+
+    resultText.innerText =
+        "Healthy Range";
+
+    resultText.style.color =
+        "#7da67d";
+
+};
